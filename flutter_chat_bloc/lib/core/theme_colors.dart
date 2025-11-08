7de691a1be70a96166d6a3160a051f9ee6d93386
afidhu <afidhumatata81@gmail.com>
2025-11-09 01:12:48 +0300

    first commit add tutorial2 add CUBIT Fevorate  add chat

---
+++ flutter_chat_bloc/lib/core/theme_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static const LinearGradient appBarBackground =  LinearGradient(
+     begin: Alignment.center,
+       colors: [
+         Colors.redAccent,
+         Colors.orangeAccent,
+         Colors.yellowAccent,
+         Colors.greenAccent,
+         Colors.blueAccent,
+       ]
+   );
+  static const Color appBarTitle = Colors.white;
+  static const Color appBarIcon = Color(0xFFFFFFFF);
+
+}
